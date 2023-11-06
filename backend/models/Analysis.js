6d5180440f1d2cf6218b77c17183f4bd22c468a2
Ian Kamweru 24 <ikamweru24@rlinux-cosc-257-node03.amherst.edu>
2023-11-06 00:23:13 -0500

    add analysisid to analysis model

--- backend/models/Analysis.js
+++ backend/models/Analysis.js
@@ -1,5 +1,6 @@
 class Analysis {
-  constructor(analysis_type, sample_name, project_name) {
+  constructor(analysis_id, analysis_type, sample_name, project_name) {
+    this.analysis_id = analysis_id;
     this.analysis_type = analysis_type;
     this.sample_name = sample_name;
     this.project_name = project_name;
