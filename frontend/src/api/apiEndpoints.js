a73e779dfd517ba5da3181cccb7a4f5ae6f179c0
Ian Kamweru <ikamweru24@amherst.edu>
2023-11-13 00:04:50 -0500

    fix api endpoints

--- frontend/src/api/apiEndpoints.js
+++ frontend/src/api/apiEndpoints.js
@@ -7,18 +7,18 @@ export const PROJECTS_API = {
 
 export const SAMPLES_API = {
   CREATE_SAMPLE: `${API_BASE}/samples`,
-  GET_BY_PROJECT: (projectName) => `${API_BASE}/samples?projectName=${projectName}`,
+  GET_BY_PROJECT: (projectName) => `${API_BASE}/samples/${projectName}`,
 };
 
 export const ANALYSES_API = {
   CREATE_ANALYSIS: `${API_BASE}/analyses`,
   GET_BY_SAMPLE: (sampleName, projectName) =>
-    `${API_BASE}/analyses?projectName=${projectName}&sampleName=${sampleName}`,
+    `${API_BASE}/analyses/${projectName}/${sampleName}`,
 };
 
 export const MAPS_API = {
   CREATE: (analysisId, sampleName, projectName) =>
-    `${API_BASE}/maps?analysisId=${analysisId}&sampleName=${sampleName}&projectName=${projectName}`,
+    `${API_BASE}/maps/${analysisId}/${sampleName}/${projectName}`,
   GET_BY_ANALYSIS: (analysisId, sampleName, projectName) =>
-    `${API_BASE}/maps?analysisId=${analysisId}&sampleName=${sampleName}&projectName=${projectName}`,
+    `${API_BASE}/maps/${analysisId}/${sampleName}/${projectName}`,
 };
