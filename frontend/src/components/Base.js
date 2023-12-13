d9f8b5d2da9ad0a1fc45d83b1fbba61f8def9785
Guilherme Santos Rocha 25 <gsantosrocha24@rlinux-cosc-257-node03.amherst.edu>
2023-12-12 21:49:27 -0500

    Extra CSS styling

--- frontend/src/components/Base.js
+++ frontend/src/components/Base.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import "./Base.css"
+import "./Base.css";
+import myImage from '/home/gsantosrocha24/Electron-Microscope-Lab-DB/frontend/src/components/logo.png'; // Path to your image file
 
-function Base(){
-    return(
-<div className="Home">
-<header className="Home-header">
-<h1>Beneski EML DataHub</h1>
-</header>
-</div>
-)
+function Base() {
+    return (
+        <div className="Home">
+            <header className="Home-header">
+                <img src={myImage} alt="Descriptive Alt Text" className="Home-logo" />
+                <div className="Home-header-text">
+                    <h1>Beneski EML DataHub</h1>
+                </div>
+            </header>
+        </div>
+    );
 }
+
+
 export default Base;
